406c1b1f470539baea190fe1654579bc0fd87c1f
matthew.zhong <34210326+mzhren@users.noreply.github.com>
2022-08-19 15:48:12 +0800

    add bilibili link

--- src/Footer.js
+++ src/Footer.js
@@ -10,8 +10,8 @@ const Footer = () => {
                 <a href="https://github.com/mzhren/xiaomi-leica.git" className='text-sm flex mr-4 hover:text-green-500' target={"_blank"} rel="noreferrer">
                     <img src={GithubIcon} className="w-4 h-4 mr-1" alt='源码下载' />源码下载</a>
 
-                <a href="https://github.com/mzhren/xiaomi-leica.git" className='text-sm flex  hover:text-green-500' target={"_blank"} rel="noreferrer">
-                    <img src={Bilibili} className="w-4 h-4 mr-1  hover:text-green-500" alt='源码解析' />视频讲解</a>
+                <a href="https://www.bilibili.com/video/BV1Md4y1N7tw" className='text-sm flex  hover:text-green-500' target={"_blank"} rel="noreferrer">
+                    <img src={Bilibili} className="w-4 h-4 mr-1  hover:text-green-500" alt='源码解析' />视频演示</a>
             </span>
 
             <div className="block md:max-w-xs my-5 mx-auto flex justify-center"><a href="http://mp.weixin.qq.com/profile?src=3&timestamp=1660316359&ver=1&signature=XVs6zpberOz8t58nr5RenE9jhxvn7e8ORqJgEU67YYN6t2AyVfyOhicrOZxsWppXZ26cS0cfPWJg3gPkXonv1Q==" target="_blank" rel="noreferrer"><img src={mzhren_wechat} alt="" /></a></div>
